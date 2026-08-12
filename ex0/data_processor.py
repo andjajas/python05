aaedfb24fbb686b41633257a4211b86381181514
Andre Djajasaputra <andjajas@f0r3s9.codam.nl>
2026-08-12 13:08:01 +0200

    slight update on ex0

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class DataProcessor(ABC):
     @abstractmethod
-    def validate(self):
+    def validate(self, data: Any) -> bool:
         pass
 
     @abstractmethod
-    def ingest(self):
+    def ingest(self, data: Any) -> None:
         pass
     
-    def output(self):
+    def output(self) -> tuple[int, str]:
         pass
 
 
 class NumericProcessor(DataProcessor):
-    def validate(self):
+    def validate(self, data: Any) -> bool:
         pass
 
-    def ingest(self):
-        pass
+    def ingest(self, num_data: int | float | list[int] | list[float]) -> None:
+        if isinstance(num_data, )
 
 
 class TextProcessor(DataProcessor):
-    def validate(self):
+    def validate(self, data: Any) -> bool:
         pass
 
     def ingest(self):
@@ -32,8 +33,14 @@ class TextProcessor(DataProcessor):
 
 
 class LogProcessor(DataProcessor):
-    def validate(self):
+    def validate(self, data: Any) -> bool:
         pass
 
     def ingest(self):
         pass
+
+
+# the ingest methods in the specialized classes will have their own
+# specific signatures to match the types they expect.
+# In case the user does not validate the data before calling ingest,
+# and provides invalid data, an exception must be raised.
