3f2007f99fba15a1b435354487db72493f4aceac
Andre Djajasaputra <andjajas@f0r5s7.codam.nl>
2026-08-17 09:15:24 +0200

    start ex1, copied classes from ex0

---
+++ ex1/data_stream.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self.queue: list[tuple[int, str]] = []
+        self.counter: int = 0
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        pass
+
+    def output(self) -> tuple[int, str]:
+        return self.queue.pop(0)
+
+
+class NumericProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, (int, float)):
+            return True
+        elif isinstance(data, list):
+            return all(isinstance(elem, (int, float)) for elem in data)
+        else:
+            return False
+
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if isinstance(data, list):
+            data_list = data
+        else:
+            data_list = [data]
+        for elem in data_list:
+            if self.validate(elem):
+                elem_str = str(elem)
+                elem_tup = (self.counter, elem_str)
+                self.queue.append(elem_tup)
+                self.counter += 1
+            else:
+                raise TypeError("Improper numeric data")
+
+
+class TextProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        elif isinstance(data, list):
+            return all(isinstance(elem, str) for elem in data)
+        else:
+            return False
+
+    def ingest(self, data: str | list[str]) -> None:
+        if isinstance(data, list):
+            data_list = data
+        else:
+            data_list = [data]
+        for elem in data_list:
+            if self.validate(elem):
+                elem_tup = (self.counter, elem)
+                self.queue.append(elem_tup)
+                self.counter += 1
+            else:
+                raise TypeError("Improper text data")
+
+
+class LogProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if (isinstance(data, dict)
+                and all(isinstance(elem, str) for elem in data.keys())
+                and all(isinstance(elem, str) for elem in data.values())):
+            return True
+        elif isinstance(data, list):
+            return all(self.validate(elem) for elem in data)
+        else:
+            return False
+
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
+        if isinstance(data, list):
+            data_list = data
+        else:
+            data_list = [data]
+        for elem in data_list:
+            if self.validate(elem):
+                elem_str = ": ".join(elem.values())
+                elem_tup = (self.counter, elem_str)
+                self.queue.append(elem_tup)
+                self.counter += 1
+            else:
+                raise TypeError("Improper dict data")
+
+
+if __name__ == "__main__":
